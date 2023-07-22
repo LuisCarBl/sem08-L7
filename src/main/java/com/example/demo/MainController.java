1376a5a81b0aec9f9eae15d8c1ca670e88ed8cb1
LuisCarBl <134304449+LuisCarBl@users.noreply.github.com>
2023-07-22 22:15:44 +0000

    pal git

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -4,18 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import java.util.List;
-import java.util.Map;
-import java.lang.String;
-import java.lang.Object;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import java.lang.String;
+
 
 @Controller	
 @RequestMapping(path="/")
